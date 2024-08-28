32c589efc86091adb7c9926d30728bc3847a4bc8
Samuel Forrest <samueljforrest@outlook.com>
2024-08-28 22:09:49 +0100

    normal commit

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { View } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default WelcomeScreen;
