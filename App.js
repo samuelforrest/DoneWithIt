21b053ea6637a9d27b36a6dc4e940aa6cd1779c2
Samuel Forrest <samueljforrest@outlook.com>
2024-08-28 21:44:26 +0100

    normal commit

--- App.js
+++ App.js
@@ -7,21 +7,20 @@ export default function App() {
       backgroundColor: "#fff",
       flex: 1, //This will make the view grow to fill the remaining space.
       flexDirection: "row", // THis will make the containers flex horizontally.
-      flexDirection: "column-reverse",
-      flexDirection: "row-reverse",
-      
+      justifyContent: "center",
+      alignItems: "baseline",
     }}>
 
       <View style={{
         backgroundColor: "dodgerblue",
         width: 100,
-        height: 100,
+        height: 300,
       }}></View>
 
       <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 100,
+        height: 200,
       }}></View>
 
       <View style={{
