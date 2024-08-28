1abc26fbf783c7110aed4f8b1ae30f66e18cef1f
Samuel Forrest <samueljforrest@outlook.com>
2024-08-28 22:02:46 +0100

    normal commit

--- App.js
+++ App.js
@@ -9,28 +9,31 @@ export default function App() {
       flexDirection: "row", // THis will make the containers flex horizontally.
       justifyContent: "center", // Main Axis
       alignItems: "center", // Secondary Axis
-      flexWrap: "wrap",
+      
       alignContent: "center",
     }}>
 
       <View style={{
         backgroundColor: "dodgerblue",
-        
         height: 100,
-        flexBasis: 100, //flexBasis can map to width or height
-        flexGrow: 1, //flexGrow is very similar to flex.
+        width: 100,
+        position: "relative",
       }}></View>
 
       <View style={{
         backgroundColor: "gold",
         width: 100,
         height: 100,
+        left: 20,
+        top: 20, // Change relative positoning
+        position: "absolute", // Gold is now positioned relative to its parent's view
       }}></View>
 
       <View style={{
         backgroundColor: "tomato",
         width: 100,
         height: 100,
+        position: "relative",
       }}></View>
 
 
