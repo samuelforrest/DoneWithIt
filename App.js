3bce22193b57e0e11ebeea4dfcc90e35178b6f9c
Samuel Forrest <samueljforrest@outlook.com>
2024-08-28 22:26:39 +0100

    normal commit

--- App.js
+++ App.js
@@ -3,6 +3,5 @@ import { View, StyleSheet, Text, ImageBackground, } from "react-native";
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <WelcomeScreen />
+  return <WelcomeScreen />;
 }
-      
