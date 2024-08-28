b141ccd6e27bcc8475970bef75b2be76a5878278
Samuel Forrest <samueljforrest@outlook.com>
2024-08-28 21:29:39 +0100

    deleted old code, reset

--- App.js
+++ App.js
@@ -1,45 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,
-  Text,
-  Alert,
-  Image,
-  Button,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  View,
-  Dimensions, 
-  SafeAreaView 
-} from 'react-native';
-
-
+import { View } from "react-native";
 
 export default function App() {
-
-  console.log(Dimensions.get("screen"));
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: "50%",
-        height: 70,
-      }}
-      ></View>
-    </SafeAreaView>
-  
-    )
+
+  );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    // The view  is flexible, it will grow to fill the entire screen
-    flex: 1,
-    // Same kinda colours in CSS
-    backgroundColor: 'lightskyblue',
-
-    //danger
-
-
-  },
-});
